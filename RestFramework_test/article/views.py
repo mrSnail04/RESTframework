936fa18eec017e2524e30acf8c97a1b2349db8b6
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-05-21 09:34:16 +0300

    upd refactoring

--- RestFramework_test/article/views.py
+++ RestFramework_test/article/views.py
@@ -8,13 +8,14 @@ from .serializers import ArticleSerializer
 
 
 class ArticleView(APIView):
-
+    """ Метод get служит для получения всех артиклей из БД """
     def get(self, request):
         articles = Article.objects.all()
         # Параметр many сообщает сериализатору,что он будет сериализовать несколько статей
         serializer = ArticleSerializer(articles, many=True)
         return Response({"articles": serializer.data})
 
+    """ Метод post служит для создания нового артикля в БД """
     def post(self, request):
         article = request.data.get('article')
         serializer = ArticleSerializer(data=article)
@@ -22,6 +23,7 @@ class ArticleView(APIView):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
 
+    """ Метод put служит для изменения артикля в БД """
     def put(self, request, pk):
         saved_article = get_object_or_404(Article.objects.all(), pk=pk)
         data = request.data.get('article')
@@ -31,6 +33,7 @@ class ArticleView(APIView):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' update successfully".format(article_saved.title)})
 
+    """ Метод delete служит для удаления артикля из БД """
     def delete (self, request, pk):
         article = get_object_or_404(Article.objects.all(), pk=pk)
         article.delete()
